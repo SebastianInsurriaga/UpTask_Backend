05cb5a6cccd37207df7a5ed93886c1151ed721ce
Sebastian <sebastianinsu@gmail.com>
2025-08-13 16:05:10 -0600

    gmail service completed

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,17 +1,13 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
-dotenv.config()
-
-const config = () => {
-    return {
-        host: process.env.SMTP_HOST,
-        port: +process.env.SMTP_PORT,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    }
-}
-
-export const transporter = nodemailer.createTransport(config());
+export const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: +process.env.SMTP_PORT,
+    secure: process.env.SMTP_PORT === '465', // true para puerto 465 (SSL), false para 587 (TLS)
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+});
