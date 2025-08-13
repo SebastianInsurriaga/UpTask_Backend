b2c744cba46ed56052991372968c55b74c73c82e
Sebastian <sebastianinsu@gmail.com>
2025-08-12 21:20:38 -0600

    fixing deployment

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,14 @@
 import {CorsOptions} from 'cors'
-import dotenv from 'dotenv';
-dotenv.config();
+export const corsConfig: CorsOptions = {
+  origin: (origin, callback) => {
+    const whitelist = [process.env.FRONTEND_URL];
 
-export const corsConfig : CorsOptions = {
-    origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
+    if (process.argv[2] === '--api') whitelist.push(undefined, null); 
 
-        if(process.argv[2] === '--api') whitelist.push(undefined)
-
-        if(whitelist.includes(origin)){
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS: origen no permitido'))
     }
+  }
 }
